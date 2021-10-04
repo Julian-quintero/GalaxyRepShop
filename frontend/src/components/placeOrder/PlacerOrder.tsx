a5db2271f692b3e34bd4d08768ecd9c8f090f5a1
Julian Quintero <darkziosj@gmail.com>
2021-10-03 20:46:14 -0500

    Place order screen with mercadoPago

--- frontend/src/components/placeOrder/PlacerOrder.tsx
+++ frontend/src/components/placeOrder/PlacerOrder.tsx
@@ -1,25 +1,27 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import {
-    SimpleGrid,
-    Box,
-    Center,
-    Button,
-    Table,
-    Thead,
-    Tbody,
-    Tr,
-    Th,
-    Td,
-    Flex,
-    Text,
-    Tfoot,
-    Divider,
-    Heading
-  } from "@chakra-ui/react";
-import { CartItems } from '../cart/CartItems';
-import { singleProduct } from '../../actions/interfaces/product';
+  SimpleGrid,
+  Box,
+  Center,
+  Button,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Flex,
+  Text,
+  Tfoot,
+  Divider,
+  Heading,
+  TableCaption,
+  Container,
+} from "@chakra-ui/react";
+import { CartItems } from "../cart/CartItems";
+import { product, singleProduct } from "../../actions/interfaces/product";
 import { useDispatch, useSelector } from "react-redux";
-import axios from 'axios';
+import axios from "axios";
 
 declare global {
   interface Window {
@@ -28,59 +30,212 @@ declare global {
 }
 
 interface RootState {
-  rootState: { cart: { cartItems: {} } };
-  cartReducer: {};
+  rootState: { cart: cartAddItem[] };
+  cartReducer: {shippingAddress: formData};
+}
+
+interface cartAddItem {
+  product: string;
+  name: string;
+  image: string;
+  price: number;
+  countInStock: number;
+  qty: number;
 }
 
 
 
-
+interface formData {
+  address: string;
+  city: string;
+  postalC: string;
+  country: string;
+}
 
 export const PlacerOrder = () => {
-
-
-  const FORM_ID = 'payment-form';
+  const FORM_ID = "payment-form";
 
   const [preferenceId, setPreferenceId] = useState<null | string>(null);
-  
-
-
 
 
   useEffect(() => {
     // luego de montarse el componente, le pedimos al backend el preferenceId
-    axios.post('/api/payments/mercadopago').then((item:any) => {
-      setPreferenceId(item.data.idpago)
-
-      
+    axios.post("/api/payments/mercadopago").then((item: any) => {
+      setPreferenceId(item.data.idpago);
     });
   }, []);
 
-  useEffect(() => {
-    if (preferenceId) {
-      // con el preferenceId en mano, inyectamos el script de mercadoPago
-      console.log(preferenceId)
-      const script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src =
-      'https://www.mercadopago.com.co/integrations/v1/web-payment-checkout.js';
-    script.setAttribute('data-preference-id', preferenceId);
+
+  async function loadScript(preferenceId: string) {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.async = true;
+    script.src =
+      "https://www.mercadopago.com.co/integrations/v1/web-payment-checkout.js";
+    script.setAttribute("data-preference-id", preferenceId);
     const form = document.getElementById(FORM_ID);
     form?.appendChild(script);
+    script.onload = function () {
+      let button = document.querySelector(".mercadopago-button");
+
+      if (button) {
+        button.innerHTML = "Continue to Checkout";
+      }
+    };
+
+   
+  }
+
+  useEffect(() => {
+    if (preferenceId) {
+
+
+      loadScript(preferenceId);
     }
   }, [preferenceId]);
 
+  const cart: cartAddItem[] = useSelector(
+    (state: RootState) => state.rootState.cart
+  );
+
+  const shippingAddress: formData = useSelector(
+    (state: RootState) => state.cartReducer.shippingAddress
+  );
+
+  
+
+  
+
+  const itemsPrice = cart.reduce((acc, item) => acc + item.price * item.qty, 0);
 
 
-    const cart: any = useSelector((state: RootState) => state.rootState.cart);
+  return (
+    <>
+      <SimpleGrid columns={[1, 1, 1, 2]} spacing={10} m={10}>
+        <Box>
+          <Text
+            fontSize="3xl"
+            align="center"
+            color="teal.700"
+            textAlign="center"
+            as={"span"}
+            position={"relative"}
+            _after={{
+              content: "''",
+              width: "full",
+              height: "10%",
+              position: "absolute",
+              bottom: 1,
+              left: 0,
+              bg: "teal",
+              zIndex: -1,
+            }}
+          >
+            SHIPPING ADDRESS
+          </Text>
 
+          <Heading size="sm" fontWeight="extrabold" mt="6" mb="2">
+            Address: { shippingAddress.address} { shippingAddress.city} { shippingAddress.country} { shippingAddress.postalC}
+          </Heading>
 
+          <Divider></Divider>
 
+          <Text
+            fontSize="3xl"
+            align="center"
+            color="teal.700"
+            textAlign="center"
+            as={"span"}
+            position={"relative"}
+            _after={{
+              content: "''",
+              width: "full",
+              height: "10%",
+              position: "absolute",
+              bottom: 1,
+              left: 0,
+              bg: "teal",
+              zIndex: -1,
+            }}
+          >
+            PAYMENT METHOD
+          </Text>
 
-    return (
-    
-        <form id={FORM_ID} method="GET" />
-     
-             
-    )
-}
+          <Heading size="sm"  mt="6" mb="2">
+            Method: MercadoPago
+          </Heading>
+
+          <Divider></Divider>
+
+          <Text
+            fontSize="3xl"
+            align="center"
+            color="teal.700"
+            textAlign="center"
+            as={"span"}
+            position={"relative"}
+            _after={{
+              content: "''",
+              width: "full",
+              height: "10%",
+              position: "absolute",
+              bottom: 1,
+              left: 0,
+              bg: "teal",
+              zIndex: -1,
+            }}
+          >
+            ORDER ITEMS
+          </Text>
+
+          <Box w="100%" color="white">
+            <Flex direction="column" alignItems="center" justify="space-evenly">
+              {cart ? (
+                cart.map((product: singleProduct) => (
+                  <CartItems
+                    key={product.name}
+                    placeOrder={true}
+                    product={product}
+                  ></CartItems>
+                ))
+              ) : (
+                <p>No hay objetos en el carrito</p>
+              )}
+            </Flex>
+          </Box>
+        </Box>
+
+        <Container borderWidth={2} borderRadius={5} w={300} h={300}>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>ORDER SUMMARY</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Items: ${itemsPrice}</Td>
+              </Tr>
+              <Tr>
+                <Td>Tax: ${(itemsPrice * 0.15).toFixed(2)}</Td>
+              </Tr>
+              <Tr>
+                <Td>Total: ${(itemsPrice * 0.15 + itemsPrice).toFixed(2)} </Td>
+              </Tr>
+            </Tbody>
+            <Tfoot>
+              <Tr>
+                <Th>
+                  {" "}
+                  <div>
+                    <form id={FORM_ID} method="GET" />
+                  </div>
+                </Th>
+              </Tr>
+            </Tfoot>
+          </Table>
+        </Container>
+      </SimpleGrid>
+    </>
+  );
+};
