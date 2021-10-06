3bd5297fdf1e98eeae9b3f2dd1c9235741573a67
Julian Quintero <darkziosj@gmail.com>
2021-10-05 22:23:20 -0500

    Order routes and controller created

--- backend/controller/payController.js
+++ backend/controller/payController.js
@@ -1,26 +1,74 @@
 import mercadopago from "mercadopago";
 // Agrega credenciales
 import asyncHandler from "express-async-handler";
+import Order from "../models/orderModel.js";
 mercadopago.configure({
   access_token:
     "TEST-7008670002521059-100302-519578625e5c99d54cf860450a8d8df3-834607133",
 });
 
 const payment = asyncHandler(async (req, res) => {
+
+  console.log(req.user)
+
+ const {
+  orderItems,
+  shippingAddress,
+  paymentMethod,
+  itemsPrice,
+  taxPrice,
+  shippingPrice,
+  totalPrice,
+} = req.body;
+
+
+try {
+
+  if (orderItems && orderItems.length === 0) {
+    //res.status(400);
+    throw new Error("No order items");
+    return;
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+  
+    const createdOrder = await order.save();
+    //res.status(201).json(createdOrder);
+  }
+  
+  
+} catch (error) {
+  console.log('error at create order',error)
+  throw new Error("Couldn't create order");
+  
+}
+
+
+
+
+
+
+ const total = (itemsPrice * 0.15 + itemsPrice).toFixed(2)
+
   let preference = {
     external_reference: '1234',
-    notification_url:`https://49ea-179-15-47-71.ngrok.io/api/payments/mercadopago/ipn`,
+    notification_url:`https://cdfc-179-15-47-71.ngrok.io/api/payments/mercadopago/ipn`,
     items: [
-        {
-            title: "Mi producto",
-            unit_price: 10000,
-            quantity: 1,
-          },
+    
           {
-            title: "Mi producto2",
-            unit_price: 10000,
+            title:"Items galaxy shop (COP currency)",
+            unit_price: Math.floor(total)*3780,
             quantity: 1,
           },
+         
     ],
   };
 
--- backend/routes/payRoutes.js
+++ backend/routes/payRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { ipn } from '../controller/ipnController.js'
 import { payment } from '../controller/payController.js'
+import { protect} from '../middleware/authMiddleware.js'
 const router = express.Router()
-router.post('/mercadopago',payment)
+router.route("/mercadopago").post(protect,payment)
 router.post('/mercadopago/ipn',ipn)
 
 export default router
